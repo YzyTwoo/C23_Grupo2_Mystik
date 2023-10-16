40e24641747fc64025202fbca082cbdae526f0a9
Tiziano Orieta <tiziorieta@gmail.com>
2023-10-16 19:49:26 -0300

    cree rutas en app.js

--- mystikweb/app.js
+++ mystikweb/app.js
@@ -6,6 +6,18 @@ app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/index.html"))
 })
 
+app.get("/coleccion", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/coleccion.html"))
+})
+
+app.get("/tienda", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/tienda.html"))
+})
+
+app.get("/politicas-devolucion", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/politicas-devolucion.html"))
+})
+
 app.use(express.static("public"))
 
 app.listen(3030, console.log("Servidor corriendo en puero 3030"));
