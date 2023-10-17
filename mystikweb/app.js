2f23c377b273be81203f7bbda9af3a9cff94a56c
Tiziano Orieta <tiziorieta@gmail.com>
2023-10-16 21:08:44 -0300

    realicé la estructura coleccion

--- mystikweb/app.js
+++ mystikweb/app.js
@@ -4,19 +4,19 @@ const path = require('path')
 
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/index.html"))
-})
+});
 
 app.get("/coleccion", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/coleccion.html"))
-})
+});
 
 app.get("/tienda", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/tienda.html"))
-})
+});
 
 app.get("/politicas-devolucion", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/politicas-devolucion.html"))
-})
+});
 
 app.use(express.static("public"))
 
