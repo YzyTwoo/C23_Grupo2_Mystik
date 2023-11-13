3fc1a909892b3690fb66b0339f2961682c1215f1
Tiziano Orieta <tiziorieta@gmail.com>
2023-11-13 14:55:38 -0300

    estructura html

--- ESTRUCTURAWEB/app.js
+++ ESTRUCTURAWEB/app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
+app.get('/detalle-producto', (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/detalle-producto.html"))
+});
+
 app.use(express.static("public"));
 
 app.listen(3030, console.log('Servidor corriendo en PUERTO 3030'));
