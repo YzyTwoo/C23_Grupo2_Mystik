1ca28005c9d533f2de9bced3b8b42f48e04b102a
FabianMedina28 <medinafabian.2004@gmail.com>
2023-11-16 19:51:04 -0300

    subiendo la estructura web

--- ESTRUCTURAWEB/app.js
+++ ESTRUCTURAWEB/app.js
@@ -5,6 +5,10 @@ const path = require('path')
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/index.html"));
 });
+app.get('/carrito', (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/carrito.html"));
+});
+
 
 app.use(express.static("public"));
 
