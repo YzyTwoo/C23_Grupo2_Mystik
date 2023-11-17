2d4e1c55fd7ab34d00162fd7d6391991087bfd43
AyelenCoppaVidela <ayelencoppav@gmail.com>
2023-11-17 00:27:34 -0300

    aplique responsive(incompleto)

--- ESTRUCTURAWEB/app.js
+++ ESTRUCTURAWEB/app.js
@@ -3,7 +3,7 @@ const app = express()
 const path = require('path')
 
 app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, "/views/index.html"));
+    res.sendFile(path.join(__dirname, "/views/footer.html"));
 });
 
 app.use(express.static("public"));
