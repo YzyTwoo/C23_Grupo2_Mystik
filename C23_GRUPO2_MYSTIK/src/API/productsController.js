c317d1b443b3aaabe98baccc2d507932d1c95c0f
LautaroContreras <LautaroContreras1998@gmail.com>
2024-04-06 15:47:03 -0300

    refactorizando apis

--- C23_GRUPO2_MYSTIK/src/API/productsController.js
+++ C23_GRUPO2_MYSTIK/src/API/productsController.js
@@ -1,4 +1,4 @@
-
+/* 
 const db = require('../database/models');
 
 
@@ -35,7 +35,94 @@ const db = require('../database/models');
   }
 }
 
-module.exports = productsController
+module.exports = productsController */
 
+
+const db = require("../database/models");
+
+const modelResponseProduct = {
+  attributes : {
+    exclude : [
+      'createdAt', 
+      'updatedAt', 
+      'categorias_id',
+      'talles_id',
+      'colecciones_id',
+      'colores_id',
+      'imagen_id' 
+    ]
+  },  
+  include: [
+    {
+        association : 'categoria',
+        attributes : ['nombre_categoria']
+    },
+    {
+      association : 'talle',
+      attributes : ['nombre_talle']
+    },
+    {
+      association : 'coleccion',
+      attributes : ['nombre_coleccion']
+    },
+    {
+      association : 'color',
+      attributes : ['nombre_color']
+    },
+ /*    {
+      association : 'imagenes',
+      attributes : ['name']
+    }  */
+  ] 
+}
+
+module.exports = {
+  allProducts : async (req, res) => {
+    try {
+      const { count, rows} = await db.Producto.findAndCountAll(modelResponseProduct)
+
+      const products = rows.map(product => {
+        return {
+          ...product.dataValues,
+          detail : `${req.protocol}://${req.get('host')}/products/${product.id}`
+        }
+      })
+
+      return res.status(200).json({
+        ok : true,
+        count,
+        products
+    })
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok : false,
+        msg : error.message || 'Error al iniciar API'
+    })
+  }
+  },
+  productByID: async (req, res) => {
+    try {
+      const product = await db.Producto.findByPk(req.params.id, modelResponseProduct)
+
+      const productCustom = {
+        ...product.dataValues,
+        categoria : product.categoria.nombre,
+        talle : product.talle.nombre,
+        coleccion : product.coleccion.nombre, 
+        color : product.colores.nombre,
+       /*  imagenes : `${req.protocol}://${req.get('host')}/images/products/${product.imagen}` */ 
+      }
+
+      return res.status(200).json({
+        ok : true,
+        product : productCustom
+      })
+    } catch (error) {
+      return res.status(error.status || 500).json({
+        ok : false,
+        msg : error.message || 'Error al iniciar API'
+    })}
+  }
+}
 
 
